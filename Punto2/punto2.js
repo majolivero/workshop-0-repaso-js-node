a89f4d6cc74242fa709ba1193be2b53f74b51281
majolivero <majolivero28@gmail.com>
2024-07-05 11:38:21 -0500

    Punto 2 terminado

--- Punto2/punto2.js
+++ Punto2/punto2.js
@@ -1,5 +1,5 @@
 class Note{
-    constructor(id, description, status = "IMPORTANTE"){
+    constructor(id, description, status = false){
         this.id = id;
         this.description = description;
         this.status = status;
@@ -31,8 +31,33 @@ class NoteManager{
         this.renderNotes();
     }
 
+    editNote(item){
+        try{
+            let inputNote = document.getElementById('new-note').value;
+            this.notes.find(note => note.id === item.id).description = inputNote;
+            this.saveNotes();
+            this.renderNotes();
+        }catch (error){
+            console.error('Se produjo un error', error);
+        }
+    }
+
+    toggleNoteStatus(id){
+        const note = this.notes.find(note => note.id === id);
+        console.log(note);
+        if(note){
+            note.status = !note.status;
+            this.saveNotes();
+            this.renderNotes();
+        }
+    }
+
     saveNotes() {
         localStorage.setItem('notes', JSON.stringify(this.notes));
+    }
+
+    loadNotes(){
+        this.renderNotes();
     }
 
     renderNotes() {
@@ -41,7 +66,10 @@ class NoteManager{
         this.notes.forEach(note => {
             const item = document.createElement('li');
             item.textContent = note.description;
-            item.className = note.status ? 'IMPORTANTE' : '';
+            item.className = note.status ? 'importante' : '';
+            const statusButton = document.createElement('button');
+            statusButton.textContent = 'Cambiar Estado';
+            statusButton.addEventListener('click', (e) => this.toggleNoteStatus(note.id));
 
             const deleteButton = document.createElement('button');
             deleteButton.textContent = 'Eliminar';
@@ -50,11 +78,33 @@ class NoteManager{
                 this.deleteNote(note.id);
             });
 
-            item.appendChild('deleteButton');
-            taskList.appendChild(item);
-        })
+            const editButton = document.createElement('button');
+            editButton.textContent = 'Editar';
+            editButton.addEventListener('click',(e) => {
+                e.stopPropagation();
+                this.editNote(note);
+            });
+
+            item.appendChild(editButton);
+            item.appendChild(deleteButton);
+            item.appendChild(statusButton);
+            noteList.appendChild(item);
+
+        });
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const noteManager = new NoteManager();
+
+    document.getElementById("add-note").addEventListener('click', (e) => {
+        const newNote = document.getElementById("new-note").value;
+        if(newNote){
+            noteManager.addNote(newNote);
+            document.getElementById('new-note').value = '';
+        }
+    });
+});
 
 
 
