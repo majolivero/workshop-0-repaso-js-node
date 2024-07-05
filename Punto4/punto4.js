e1466d51853ba514de818554fc870b3f041cd5e5
majolivero <majolivero28@gmail.com>
2024-07-05 16:45:59 -0500

    Punto 4 Consumo de API de productos terminado

--- Punto4/punto4.js
+++ Punto4/punto4.js
@@ -1,10 +1,36 @@
 const botonCargarProductos = document.querySelector("#fetch-products");
+const InputBuscarProducto = document.querySelector("#searchProduct");
+const botonFiltrarProductos = document.querySelector("#filtered-products");
+
 botonCargarProductos.addEventListener("click",(e) => {
     fetchProducts();
 });
 
+botonFiltrarProductos.addEventListener("click",(e) => {
+    filteredProducts();
+})
+
+
+//Consumo de API de Platzi Fake Store API para mostrar productos
 const fetchProducts = () => {
     fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Network response was not ok' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(products => {
+        displayProducts(products);
+    })
+    .catch(error => {
+        displayError(error);
+    });
+};
+
+//Consumo de API de Platzi Fake Store API para filtrar por titulo
+const filteredProducts = () => {
+    fetch(`https://api.escuelajs.co/api/v1/products/?title=${InputBuscarProducto.value}`)
     .then(response => {
         if(!response.ok){
             throw new Error('Network response was not ok' + response.statusText);
@@ -33,3 +59,4 @@ const displayError = (error) => {
     const errorMessage = document.getElementById("error-message");
     errorMessage.textContent = `Error: ${error.message}`;
 };
+
